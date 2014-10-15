70cf8536ca8ceb079dbac17c26f94a4c2b14eb5c
viandrada <victoria.andrada@gmail.com>
2014-10-15 20:57:54 -0200

    Agregando el proyecto web

---
+++ src/com/serviciorest/ServicioPersonas.java
@@ -0,0 +1,43 @@
+package com.serviciorest;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.serviciorest.modelo.Persona;
+import com.serviciorest.repositorio.PersonasStub;
+
+//personas
+@Path("personas") 
+public class ServicioPersonas {
+	 
+	private PersonasStub personaStub = new PersonasStub();
+	
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public List<Persona> getPersonas()
+	{
+		return personaStub.getPersonas();
+	}
+		
+	//2
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Path("{personaId}")
+	public Persona getPersona(@PathParam("personaId") String personaId)
+	{
+		return personaStub.getPersona(personaId);
+	}
+	
+	/*@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getPersonasHTML()
+	{
+		return "<p>Personas</p>";
+	}*/
+
+}
